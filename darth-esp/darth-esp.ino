1f5722772317220f7e1881610c2b9b0e05b0147e
matt kocaj <mkocaj@hotmail.com>
2016-04-14 21:46:17 +0800

    adding notes for program overview

--- darth-esp/darth-esp.ino
+++ darth-esp/darth-esp.ino
@@ -1,3 +1,13 @@
+// broad overview:
+// * timer to invoke method checkBuild() every 5s
+// * post to TeamCity api for http://teamcity-path.com/httpAuth/app/rest/buildTypes/id:someid/builds?count=1
+// inspect response for "status":"SUCCESS"
+// if success set GPIO2 HIGH for GREEN
+// else set GPIO2 LOW for RED (default on startup)
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
